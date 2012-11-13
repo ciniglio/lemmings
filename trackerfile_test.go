5d298f5e9dea849a45214f8f5cb1e7c77071549d
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-11-13 11:05:15 -0500

    Added more tests.

--- trackerfile_test.go
+++ trackerfile_test.go
@@ -10,4 +10,19 @@ func Test_DecodeTracker_1(t *testing.T) {
 	if ti.announce != "http://tracker.broadcasthe.net:34000/os6ch62cq8f1jftymy25e1gtpmigp0so/announce" {
 		t.Error("Failed parsing announce: %s", ti.announce)
 	}
+	if ti.pieceLength != 2097152 {
+		t.Error("Failed parsing Piece Length", ti.pieceLength)
+	}
+	if len(ti.pieces) != 1 {
+		t.Error("Failed parsing number of pieces", ti.pieces)
+	}
+	if ti.pieces[0] != "ffffffffffffffffffff" {
+		t.Error("Failed parsing pieces", ti.pieces)
+	}
+	if len(ti.files) != 1 {
+		t.Error("Failed parsing number of files", ti.files)
+	}
+	if ti.files[0].length != 1321388219 {
+		t.Error("Failed parsing file size", ti.files[0])
+	}
 }
