e094ac749f21d682fecd8d76cf60ee3da4b897dc
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-11-15 14:07:29 -0500

    Does handshake and begins talking to peers.

--- peer.go
+++ peer.go
@@ -6,9 +6,10 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"io"
 )
 
-type PeerConnection struct {
+type PeerConnectionInfo struct {
 	peer_choking    bool
 	peer_interested bool
 	am_choking      bool
@@ -27,6 +28,9 @@ type Peer struct {
 	torrent_info *TorrentInfo
 	torrent_peer *torrentPeer
 	connection   *net.TCPConn
+	connection_info *PeerConnectionInfo
+	their_id     [20]byte
+	shook_hands  bool
 }
 
 func CreatePeer(p *torrentPeer, t *TorrentInfo) *Peer {
@@ -51,8 +55,117 @@ func CreatePeer(p *torrentPeer, t *TorrentInfo) *Peer {
 	} else {
 		fmt.Println("Connected to a peer")
 	}
-	peer.initiateHandshake()
+	peer.runPeer()
 	return peer
+}
+
+func (peer *Peer) runPeer() {
+	peer.initiateHandshake()
+	fmt.Println("Sent Handshake")
+	var data []byte
+	for {
+		n := peer.readRawBytesFromConnection(&data)
+		fmt.Printf("OUTER Data has %d bytes: % X\n", len(data), data)
+		if len(data) > 0 {
+			fmt.Printf("Receieved %d bytes: % X\n", n, data)
+			if msg, _ := peer.parseHandshakeMessage(&data); msg == nil {
+				fmt.Printf("Data has %d bytes: % X\n", len(data), data)
+				peer.parseProtocolMessage(&data)
+				fmt.Printf("Data has %d bytes: % X\n", len(data), data)
+			}
+		}
+	}
+}
+
+func (peer *Peer) parseProtocolMessage(b *[]byte) {
+	m := *b
+	curpos := 0
+	switch {
+	case bytes.Compare(m[:2], []byte("\x00\x00")) == 0 :
+		// Keep alive, do nothing
+		curpos += 2
+		fmt.Printf("Keep Alive\n")
+	case bytes.Compare(m[:2], []byte("\x00\x01")) == 0 :
+		curpos += 2 + toInt(m[:2])
+		fmt.Printf("Choke/Unchoke/Interested/Uninterested")
+		peer.recieveChokeAndInterest(m[2:curpos])
+	}
+	*b = m[curpos:]
+	return
+}
+
+func (peer *Peer) recieveChokeAndInterest(b []byte) {
+	switch {
+	case bytes.Compare(b, []byte("\x00")) == 0:
+		peer.connection_info.peer_choking = true
+	case bytes.Compare(b, []byte("\x01")) == 0:
+		peer.connection_info.peer_choking = false
+	case bytes.Compare(b, []byte("\x02")) == 0:
+		peer.connection_info.peer_interested = true
+	case bytes.Compare(b, []byte("\x03")) == 0:
+		peer.connection_info.peer_interested = false
+		
+	}
+}
+
+func (p *Peer) readRawBytesFromConnection(out *[]byte) (int) {
+	readcount := 0
+	bufsize := 1024
+	//out := make([]byte, 0)
+	for {
+		data := make([]byte, bufsize)
+		n, err := p.connection.Read(data) 
+		if err != io.EOF && err != nil {
+			fmt.Printf("Read %d bytes\n", n)
+			fmt.Println("Reading from connection: ", err)
+		}
+		readcount += n
+		*out = append(*out, data[0:n]...)
+		if n < bufsize {
+			break
+		}
+	}
+	return readcount
+}
+
+func (p *Peer) parseHandshakeMessage(b *[]byte) (*HandshakeMessage, int) {
+	if p.shook_hands {
+		return nil, 0
+	}
+	m := *b
+	curpos := 0
+	message := new(HandshakeMessage)
+	message.pstrlen = m[0]
+	if message.pstrlen != byte(len("BitTorrent protocol")) {
+		return nil, 0
+	}
+	curpos += 1
+	message.pstr = m[curpos:int(m[0])+curpos]
+	fmt.Printf("Message length: %d\n", int(m[0]))
+	for i, b := range message.pstr{
+		if b != byte("BitTorrent protocol"[i]) {
+			return nil, 0
+		}
+	}
+	curpos += int(m[0])
+	curpos += 8  // Reserved bytes
+	for i, _ := range m[curpos:20+curpos]{
+		message.info_hash[i] = m[curpos:20+curpos][i]
+		if message.info_hash[i] != byte(p.torrent_info.info_hash[i]) {
+			return nil, 0
+		}
+	}
+	curpos += 20 // info_hash
+	for i, _ := range m[curpos:20+curpos]{
+		message.peer_id[i] = m[curpos:20+curpos][i]
+	}
+	curpos += 20 // peer_id
+	fmt.Printf("Message: %q\n", message.info_hash)
+	fmt.Printf("Peer: %q\n", message.peer_id)
+	
+	p.shook_hands = true
+	*b = m[curpos:]
+	return message, curpos
 }
 
 func (p *Peer) initiateHandshake() {
@@ -67,7 +180,9 @@ func (p *Peer) initiateHandshake() {
 	binary.Read(strings.NewReader(p.torrent_info.client_id),
 		binary.BigEndian, &message.peer_id)
 
-	//p.connection
+	p.connection.Write(message.bytes())
+
+	return
 }
 
 func (h *HandshakeMessage) bytes() []byte {
--- util.go
+++ util.go
@@ -3,6 +3,9 @@ package tracker
 import (
 	"math/rand"
 	"time"
+	"encoding/binary"
+	"bytes"
+	"fmt"
 )
 
 func RandomBytes(n int) []byte {
@@ -12,4 +15,13 @@ func RandomBytes(n int) []byte {
 		out[i] = byte(rand.Int())
 	}
 	return out
+}
+
+func toInt(b []byte) int {
+	var i int
+	err := binary.Read(bytes.NewReader(b), binary.BigEndian, &i)
+	if err != nil {
+		fmt.Println("Error converting to int: ", err)
+	}
+	return i
 }
